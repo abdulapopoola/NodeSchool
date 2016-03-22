cd8102d986346a91be869e7301a33a3545acd43a
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-03-22 10:50:48 -0700

    Finished combiner

--- stream-adventure/combiner.js
+++ stream-adventure/combiner.js
@@ -1,22 +1,41 @@
-var combine = require('stream-combiner')
-
-var genreMaps = {};
-function createGenre(genre) {
-    if (!genreMaps[valueType]) {
-        genreMaps[valueType] = [];
-    }
-}
-
-function group(bookName, bookGenre) { 
-    genreMaps[bookGenre].push(bookName);
-}
+var combine = require('stream-combiner');
+var split = require('split');
+var through = require('through2');
+var zlib = require('zlib')
 
 module.exports = function() {
+    var tr = through(write, end);
+    var current;
+    function write(buffer, encoding, next) {
+
+        if (buffer.length === 0) {
+            return next();
+        }
+
+        var row = JSON.parse(buffer);
+        if (row.type === 'genre') {
+            if (current) {
+                this.push(JSON.stringify(current) + '\n')
+            }
+            current = {
+                name: row.name,
+                books: []
+            };
+        } else if (row.type === 'book') {
+            current.books.push(row.name);
+        }
+        next();
+    }
+
+    function end(done) {
+        if (current) {
+            this.push(JSON.stringify(current) + '\n');
+        }
+        done();
+    }
     return combine(
-        process.stdin,
-        split,
-        // read newline-separated json,
-        // group books into genres,
-        // then gzip the output
+        split(),
+        tr,
+        zlib.createGzip()
     )
-}
+};
