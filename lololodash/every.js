1d90c0075ec879f5c6aa0fad7fa8f721f0b77709
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-03-24 10:21:11 -0700

    every and some

---
+++ lololodash/every.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var _ = require('lodash');
+
+var worker = function(items) {
+    var categories = {
+        'hot': [],
+        'warm': []
+    };
+
+    var checkTemp = function(temp) {
+        return temp >= 19;
+    };
+
+    _.forEach(items, function(item, itemName) {
+        if (_.every(item, checkTemp)) {
+            categories.hot.push(itemName);
+        } else if (_.some(item, checkTemp)) {
+            categories.warm.push(itemName);
+        }
+    });
+
+    return categories;
+};
+
+module.exports = worker;
