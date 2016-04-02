a0b9cbb13e99dd42f532254da83ae4a71f0c83d9
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-04-01 21:08:33 -0700

    filter

---
+++ functionalJavaScript/filter.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function getShortMessages(messages){
+    return messages.filter((message)=>{
+        return message.message.length < 50;
+    }).map((message)=>{
+        return message.message;
+    });
+}
+
+module.exports = getShortMessages;
