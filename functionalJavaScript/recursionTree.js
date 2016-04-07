e557a1bd9c81a8614e790f5f892c6a88ecdb5b66
AbdulFattah Popoola <abdulapopoola@yahoo.com>
2016-04-07 11:15:37 -0700

    getDependencie

---
+++ functionalJavaScript/recursionTree.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function getDependencies(tree, allDeps) {
+  allDeps = allDeps || {};
+  var dependencies = tree.dependencies || [];
+
+  Object.keys(dependencies).forEach(function(dep) {
+    var fullDepName = [dep, dependencies[dep].version].join('@');
+    allDeps[fullDepName] = fullDepName;
+    getDependencies(dependencies[dep], allDeps);
+  });
+
+  return Object.keys(allDeps).sort();
+}
+
+module.exports = getDependencies;
